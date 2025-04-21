73389fb21de9c4f1839160d14a31f5761a28c474
AdelinoBard <adelino.bard@gmail.com>
2025-04-21 14:23:35 -0300

    Inicial

---
+++ LojaEsportes_WebApp/Models/StoreDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaEsportes_WebApp.Models {
+    
+    public class StoreDbContext : DbContext {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options)
+            : base(options) { }
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
+
+/*
+A classe base DbContext fornece acesso à funcionalidade subjacente do Entity Framework Core, e a propriedade Products fornecerá acesso aos objetos Product no banco de dados. A classe StoreDbContext é derivada de DbContext e adiciona as propriedades que serão usadas para ler e gravar os dados do aplicativo. Há apenas uma propriedade por enquanto, que fornecerá acesso aos objetos Product.
+*/
